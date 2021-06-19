d73c7ef37bb453f39c5ee51deb14575ba4c2e2d2
hiltoncsj <hiltoncsj@gmail.com>
2021-06-19 13:35:05 -0300

    Adicionando exercicio 7, 8, 9

---
+++ exercicio Python8.py
@@ -0,0 +1,26 @@
+class Calculadora:
+    def __init__(self):
+        pass
+    
+    def soma(self, valor_a, valor_b):
+        return valor_a + valor_b
+
+    def subtracao(self, valor_a, valor_b):
+        return valor_a - valor_b
+
+    def multiplicacao(self, valor_a, valor_b):
+        return valor_a * valor_b
+
+    def divisao(self, valor_a, valor_b):
+        return valor_a / valor_b
+
+    def resto(self, valor_a, valor_b):
+        return valor_a % valor_b
+
+#instanciar uma classe
+calculadora = Calculadora()
+print("Soma: {}".format(calculadora.soma(5,3)))
+print("Subtração: {}".format(calculadora.subtracao(5,3)))
+print("Multiplicação: {}".format(calculadora.multiplicacao(5,3)))
+print("Divisão: {}".format(calculadora.divisao(5,3)))
+print("Resto: {}".format(calculadora.resto(5,3)))
