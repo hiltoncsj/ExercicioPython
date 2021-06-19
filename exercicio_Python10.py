1b41a80e625dc5e88a82d7ee8a6866d8ef276422
hiltoncsj <hiltoncsj@gmail.com>
2021-06-19 14:54:58 -0300

    Alteração de nome do arquivo e adiciona exercicio 10

---
+++ exercicio_Python10.py
@@ -0,0 +1,8 @@
+from exercicio_Python9 import Televisao
+
+televisao = Televisao()
+print(televisao.ligada)
+televisao.power()
+print(televisao.ligada)
+televisao.aumenta_canal()
+print(televisao.canal)
